503b6196b919c310ca748c54bf9ef83026988221
malonicester <ashish.tripathy39@gmail.com>
2023-06-25 02:29:54 +0530

    trying cicd

---
+++ src/test/java/com/spring/github_actions/SpringGithubActionsApplicationTests.java
@@ -0,0 +1,15 @@
+package com.spring.github_actions;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertEquals(true,true);
+	}
+
+}
